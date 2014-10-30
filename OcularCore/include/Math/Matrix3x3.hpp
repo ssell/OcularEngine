e8f0dfd3231b387795f235ddeb9bf192cace262c
Steven Sell <ssell@knights.ucf.edu>
2014-10-30 16:39:27 -0400

    Finished Matrix4x4; Implemented Matrix3x3

--- OcularCore/include/Math/Matrix3x3.hpp
+++ OcularCore/include/Math/Matrix3x3.hpp
@@ -40,6 +40,21 @@ namespace Ocular
         {
         public:
 
+            Matrix3x3(T const p00, T const p01, T const p02,
+                      T const p10, T const p11, T const p12,
+                      T const p20, T const p21, T const p22)
+            {
+                m_Contents[0] = p00;
+                m_Contents[1] = p01;
+                m_Contents[2] = p02;
+                m_Contents[3] = p10;
+                m_Contents[4] = p11;
+                m_Contents[5] = p12;
+                m_Contents[6] = p20;
+                m_Contents[7] = p21;
+                m_Contents[8] = p22;
+            }
+
             Matrix3x3()
             {
 
@@ -50,9 +65,373 @@ namespace Ocular
 
             }
 
+            //------------------------------------------------------------------------------
+            // OPERATORS
+            //------------------------------------------------------------------------------
+            
+            T& operator[](unsigned index)
+            {
+                if(index > 8)
+                {
+                    THROW_EXCEPTION("Out-Of-Bounds Matrix Access");
+                }
+
+                return m_Content[index];
+            }
+
+            const T& operator[](unsigned index) const
+            {
+                if(index > 8)
+                {
+                    THROW_EXCEPTION("Out-Of-Bounds Matrix Access");
+                }
+
+                return m_Content[index];
+            }
+
+            Matrix3x3<T>& operator=(Matrix3x3<T> const rhs)
+            {
+                for(unsigned i = 0; i < 9; i++)
+                {
+                    m_Contents[i] = rhs[i];
+                }
+
+                return (*this);
+            }
+
+            bool operator==(Matrix3x3<T> const rhs)
+            {
+                // TODO : need smart compare for floating point
+                return (m_Content[0] == rhs[0]) && (m_Content[1] == rhs[1]) && (m_Content[2] == rhs[2]) && 
+                       (m_Content[3] == rhs[3]) && (m_Content[4] == rhs[4]) && (m_Content[5] == rhs[5]) &&
+                       (m_Content[6] == rhs[6]) && (m_Content[7] == rhs[7]) && (m_Content[8] == rhs[8]));
+            }
+
+            bool operator!=(Matrix3x3<T> const rhs)
+            {
+                return !(*this == rhs);
+            }
+
+            Matrix3x3<T>& operator+(Matrix3x3<T> const rhs)
+            {
+                Matrix3x3<T> result(m_Content[0] + rhs[0], m_Content[1] + rhs[1],
+                                    m_Content[2] + rhs[2], m_Content[3] + rhs[3],
+                                    m_Content[4] + rhs[4], m_Content[5] + rhs[5],
+                                    m_Content[6] + rhs[6], m_Content[7] + rhs[7],
+                                    m_Content[8] + rhs[8]);
+
+                return result;
+            }
+
+            Matrix3x3<T>& operator+=(Matrix3x3<T> const rhs)
+            {
+                for(unsigned i = 0; i < 9; i++)
+                {
+                    m_Contents[i] += rhs[i];
+                }
+
+                return (*this);
+            }
+
+            Matrix3x3<T>& operator-(Matrix3x3<T> const rhs)
+            {
+                Matrix3x3<T> result(m_Content[0] - rhs[0], m_Content[1] - rhs[1],
+                                    m_Content[2] - rhs[2], m_Content[3] - rhs[3],
+                                    m_Content[4] - rhs[4], m_Content[5] - rhs[5],
+                                    m_Content[6] - rhs[6], m_Content[7] - rhs[7],
+                                    m_Content[8] - rhs[8])
+
+                return result;
+            }
+
+            Matrix3x3<T>& operator-=(Matrix3x3<T> const rhs)
+            {
+                for(unsigned i = 0; i < 9; i++)
+                {
+                    m_Contents[i] -= rhs[i];
+                }
+
+                return (*this);
+            }
+
+            Matrix3x3<T>& operator*(Matrix3x3<T> const rhs)
+            {
+                Matrix3x3<T> result;
+
+                for(unsigned i = 0; i < 9; i++)
+                {
+                    result[i] = m_Contents[i] * rhs;
+                }
+
+                return result;
+            }
+
+            Matrix3x3<T>& operator*(T const rhs)
+            {
+                Matrix3x3<T> result(m_Content[0] * rhs[0], m_Content[1] * rhs[1],
+                                    m_Content[2] * rhs[2], m_Content[3] * rhs[3],
+                                    m_Content[4] * rhs[4], m_Content[5] * rhs[5],
+                                    m_Content[6] * rhs[6], m_Content[7] * rhs[7],
+                                    m_Content[8] * rhs[8]);
+
+                return result;
+            }
+
+            Matrix3x3<T>& operator*(Vector3<T> const rhs)
+            {
+                Matrix3x3<T> result;
+
+                for(unsigned i = 0; i < 2; i++)
+                {
+                    result[(i * 3) + 0] = m_Contents[(i * 3) + 0] * rhs.x;
+                    result[(i * 3) + 1] = m_Contents[(i * 3) + 1] * rhs.y;
+                    result[(i * 3) + 2] = m_Contents[(i * 3) + 2] * rhs.z;
+                }
+
+                return result;
+            }
+
+            Matrix3x3<T>& operator*=(Matrix3x3<T> const rhs)
+            {
+                for(unsigned i = 0; i < 9; i++)
+                {
+                    m_Contents[i] *= rhs[i];
+                }
+
+                return (*this);
+            }
+
+            Matrix3x3<T>& operator*=(T const rhs)
+            {
+                for(unsigned i = 0; i < 9; i++)
+                {
+                    m_Contents[i] *= rhs;
+                }
+
+                return (*this);
+            }
+
+            Matrix3x3<T>& operator*=(Vector3<T> const rhs)
+            {
+                for(unsigned i = 0; i < 3; i++)
+                {
+                    m_Contents[(i * 3) + 0] *= rhs.x;
+                    m_Contents[(i * 3) + 1] *= rhs.y;
+                    m_Contents[(i * 3) + 2] *= rhs.z;
+                }
+
+                return (*this);
+            }
+
+            //------------------------------------------------------------------------------
+            // GETTERS
+            //------------------------------------------------------------------------------
+
+            /**
+             * \return The X-Axis rotation vector
+             */
+            Vector3<T> getXRotation()
+            {
+                return Vector3<T>(m_Contents[0], m_Contents[1], m_Contents[2]);
+            }
+
+            /**
+             * \return The Y-Axis rotation vector
+             */
+            Vector3<T> getYRotation()
+            {
+                return Vector3<T>(m_Contents[3], m_Contents[4], m_Contents[5]);
+            }
+
+            /**
+             * \return The Z-Axis rotation vector
+             */
+            Vector3<T> getZRotation()
+            {
+                return Vector3<T>(m_Contents[6], m_Contents[7], m_Contents[8]);
+            }
+
+            /**
+             * \return The element at the specified row [0-2] and colum [0-2] combination
+             */
+            T getElement(unsigned const row, unsigned const column)
+            {
+                if(row > 2)
+                {
+                    THROW_EXCEPTION("Out-Of-Bounds Matrix Row Access");
+                }
+
+                if(column > 2)
+                {
+                    THROW_EXCEPTION("Out-Of-Bounds Matrix Column Access");
+                }
+
+                return m_Contents[(row * 3) + column];
+            }
+
+            /**
+             * \return The element at the specified index [0-8]
+             */
+            T getElement(unsigned const index)
+            {
+                if(index > 8)
+                {
+                    THROW_EXCEPTION("Out-Of-Bounds Matrix Index Access");
+                }
+
+                return m_Contents[index];
+            }
+
+            //------------------------------------------------------------------------------
+            // SETTERS
+            //------------------------------------------------------------------------------
+
+            /**
+             * Sets matrix as the identity matrix.
+             */
+            void setIdentity()
+            {
+                // X-Axis Rotation
+                m_Contents[0] = static_cast<T>(1);
+                m_Contents[1] = static_cast<T>(0);
+                m_Contents[2] = static_cast<T>(0);
+
+                // Y-Axis Rotation
+                m_Contents[4] = static_cast<T>(0);
+                m_Contents[5] = static_cast<T>(1);
+                m_Contents[6] = static_cast<T>(0);
+
+                // Z-Axis Rotation
+                m_Contents[8] = static_cast<T>(0);
+                m_Contents[9] = static_cast<T>(0);
+                m_Contents[10] = static_cast<T>(1);
+            }
+
+            /**
+             * Sets the X-Axis rotation vector.
+             *
+             * \param[in] x X component of the rotation vector
+             * \param[in] y Y component of the rotation vector
+             * \param[in] z Z component of the rotation vector
+             */
+            void setXRotation(T const x, T const y, T const z)
+            {
+                m_Contents[0] = x;
+                m_Contents[1] = y;
+                m_Contents[2] = z;
+            }
+
+            /**
+             * Sets the X-Axis rotation vector.
+             *
+             * \param[in] vec The X-Axis rotation vector
+             */
+            void setXRotation(Vector3<T> const vec)
+            {
+                m_Contents[0] = vec.x;
+                m_Contents[1] = vec.y;
+                m_Contents[2] = vec.z;
+            }
+
+            /**
+             * Sets the Y-Axis rotation vector.
+             *
+             * \param[in] x X component of the rotation vector
+             * \param[in] y Y component of the rotation vector
+             * \param[in] z Z component of the rotation vector
+             */
+            void setYRotation(T const x, T const y, T const z)
+            {
+                m_Contents[3] = x;
+                m_Contents[4] = y;
+                m_Contents[5] = z;
+            }
+
+            /**
+             * Sets the Y-Axis rotation vector.
+             *
+             * \param[in] vec The Y-Axis rotation vector
+             */
+            void setYRotation(Vector3<T> const vec)
+            {
+                m_Contents[3] = vec.x;
+                m_Contents[4] = vec.y;
+                m_Contents[5] = vec.z;
+            }
+
+            /**
+             * Sets the Z-Axis rotation vector.
+             *
+             * \param[in] x X component of the rotation vector
+             * \param[in] y Y component of the rotation vector
+             * \param[in] z Z component of the rotation vector
+             */
+            void setZRotation(T const x, T const y, T const z)
+            {
+                m_Contents[6] = x;
+                m_Contents[7] = y;
+                m_Contents[8] = z;
+            }
+
+            /**
+             * Sets the Z-Axis rotation vector.
+             *
+             * \param[in] vec The Z-Axis rotation vector
+             */
+            void setZRotation(Vector3<T> const vec)
+            {
+                m_Contents[6] = vec.x;
+                m_Contents[7] = vec.y;
+                m_Contents[8] = vec.z;
+            }
+            
+            /**
+             * Sets the value of the element at the specified row/column position.
+             *
+             * \param[in] row    Row of the element [0,2]
+             * \param[in] column Column of the element [0,2]
+             * \param[in] value  Value to set
+             */
+            void setElement(unsigned const row, unsigned const column, T const value)
+            {
+                if(row > 2)
+                {
+                    THROW_EXCEPTION("Out-Of-Bounds Matrix Row Access");
+                }
+
+                if(column > 2)
+                {
+                    THROW_EXCEPTION("Out-Of-Bounds Matrix Column Access");
+                }
+
+                m_Contents[(row * 3) + column] = value;
+            }
+
+            /**
+             * Sets the value of the element at the specified index.
+             *
+             * \param[in] index Index of the element [0,8]
+             * \param[in] value Value to set
+             */
+            void setElement(unsigned const index, T const value)
+            {
+                if(index > 8)
+                {
+                    THROW_EXCEPTION("Out-Of-Bounds Matrix Index Access");
+                }
+
+                m_Contents[index] = value;
+            }
+
+            //------------------------------------------------------------------------------
+            // OPERATIONS
+            //------------------------------------------------------------------------------
+
         protected:
 
         private:
+
+            T m_Contents[9];
         };
 
     }
--- OcularCore/include/Math/Matrix4x4.hpp
+++ OcularCore/include/Math/Matrix4x4.hpp
@@ -21,6 +21,7 @@
 #include "Exception.hpp"
 #include "Matrix3x3.hpp"
 #include "Vector3.hpp"
+#include "Vector4.hpp"
 
 //------------------------------------------------------------------------------------------
 
@@ -87,9 +88,9 @@ namespace Ocular
              * \param[in] p33 Position vector w element (should be 0)
              */
             Matrix4x4(T const p00, T const p01, T const p02, T const p03,
-                T const p10, T const p11, T const p12, T const p13,
-                T const p20, T const p21, T const p22, T const p23,
-                T const p30, T const p31, T const p32, T const p33)
+                      T const p10, T const p11, T const p12, T const p13,
+                      T const p20, T const p21, T const p22, T const p23,
+                      T const p30, T const p31, T const p32, T const p33)
             {
                 // X-Axis Rotation
                 m_Contents[0]  = p00;
@@ -118,7 +119,11 @@ namespace Ocular
 
             Matrix4x4(Matrix3x3<T> const matrix)
             {
+                setIdentity();
 
+                setXRotation(matrix.getXRotation());
+                setYRotation(matrix.getYRotation());
+                setZRotation(matrix.getZRotation());
             }
 
             Matrix4x4()
@@ -155,45 +160,195 @@ namespace Ocular
                 return m_Content[index];
             }
 
+            Matrix4x4<T>& operator=(Matrix4x4<T> const rhs)
+            {
+                for(unsigned i = 0; i < 16; i++)
+                {
+                    m_Contents[i] = rhs[i];
+                }
+
+                return (*this);
+            }
+
+            bool operator==(Matrix4x4<T> const rhs)
+            {
+                // TODO : need smart compare for floating point
+                return (m_Content[0]  == rhs[0])  && (m_Content[1]  == rhs[1])  &&
+                       (m_Content[2]  == rhs[2])  && (m_Content[3]  == rhs[3])  &&
+                       (m_Content[4]  == rhs[4])  && (m_Content[5]  == rhs[5])  &&
+                       (m_Content[6]  == rhs[6])  && (m_Content[7]  == rhs[7])  &&
+                       (m_Content[8]  == rhs[8])  && (m_Content[9]  == rhs[9])  &&
+                       (m_Content[10] == rhs[10]) && (m_Content[11] == rhs[11]) &&
+                       (m_Content[12] == rhs[12]) && (m_Content[13] == rhs[13]) &&
+                       (m_Content[14] == rhs[14]) && (m_Content[15] == rhs[15]));
+            }
+
+            bool operator!=(Matrix4x4<T> const rhs)
+            {
+                return !(*this == rhs);
+            }
+
+            Matrix4x4<T>& operator+(Matrix4x4<T> const rhs)
+            {
+                Matrix4x4<T> result(m_Content[0]  + rhs[0],  m_Content[1]  + rhs[1],
+                                    m_Content[2]  + rhs[2],  m_Content[3]  + rhs[3],
+                                    m_Content[4]  + rhs[4],  m_Content[5]  + rhs[5],
+                                    m_Content[6]  + rhs[6],  m_Content[7]  + rhs[7],
+                                    m_Content[8]  + rhs[8],  m_Content[9]  + rhs[9],
+                                    m_Content[10] + rhs[10], m_Content[11] + rhs[11],
+                                    m_Content[12] + rhs[12], m_Content[13] + rhs[13],
+                                    m_Content[14] * rhs[14], m_Content[15] * rhs[15]);
+
+                return result;
+            }
+
+            Matrix4x4<T>& operator+=(Matrix4x4<T> const rhs)
+            {
+                for(unsigned i = 0; i < 16; i++)
+                {
+                    m_Contents[i] += rhs[i];
+                }
+
+                return (*this);
+            }
+
+            Matrix4x4<T>& operator-(Matrix4x4<T> const rhs)
+            {
+                Matrix4x4<T> result(m_Content[0]  - rhs[0],  m_Content[1]  - rhs[1],
+                                    m_Content[2]  - rhs[2],  m_Content[3]  - rhs[3],
+                                    m_Content[4]  - rhs[4],  m_Content[5]  - rhs[5],
+                                    m_Content[6]  - rhs[6],  m_Content[7]  - rhs[7],
+                                    m_Content[8]  - rhs[8],  m_Content[9]  - rhs[9],
+                                    m_Content[10] - rhs[10], m_Content[11] - rhs[11],
+                                    m_Content[12] - rhs[12], m_Content[13] - rhs[13],
+                                    m_Content[14] - rhs[14], m_Content[15] - rhs[15]);
+
+                return result;
+            }
+
+            Matrix4x4<T>& operator-=(Matrix4x4<T> const rhs)
+            {
+                for(unsigned i = 0; i < 16; i++)
+                {
+                    m_Contents[i] -= rhs[i];
+                }
+
+                return (*this);
+            }
+
+            Matrix4x4<T>& operator*(Matrix4x4<T> const rhs)
+            {
+                Matrix4x4<T> result;
+
+                for(unsigned i = 0; i < 16; i++)
+                {
+                    result[i] = m_Contents[i] * rhs;
+                }
+
+                return result;
+            }
+
+            Matrix4x4<T>& operator*(T const rhs)
+            {
+                Matrix4x4<T> result(m_Content[0]  * rhs[0],  m_Content[1]  * rhs[1],
+                                    m_Content[2]  * rhs[2],  m_Content[3]  * rhs[3],
+                                    m_Content[4]  * rhs[4],  m_Content[5]  * rhs[5],
+                                    m_Content[6]  * rhs[6],  m_Content[7]  * rhs[7],
+                                    m_Content[8]  * rhs[8],  m_Content[9]  * rhs[9],
+                                    m_Content[10] * rhs[10], m_Content[11] * rhs[11],
+                                    m_Content[12] * rhs[12], m_Content[13] * rhs[13],
+                                    m_Content[14] * rhs[14], m_Content[15] * rhs[15]);
+
+                return result;
+            }
+
+            Matrix4x4<T>& operator*(Vector4<T> const rhs)
+            {
+                Matrix4x4<T> result;
+
+                for(unsigned i = 0; i < 3; i++)
+                {
+                    result[(i * 4) + 0] = m_Contents[(i * 4) + 0] * rhs.x;
+                    result[(i * 4) + 1] = m_Contents[(i * 4) + 1] * rhs.y;
+                    result[(i * 4) + 2] = m_Contents[(i * 4) + 2] * rhs.z;
+                    result[(i * 4) + 3] = m_Contents[(i * 4) + 3] * rhs.w;
+                }
+
+                return result;
+            }
+
+            Matrix4x4<T>& operator*=(Matrix4x4<T> const rhs)
+            {
+                for(unsigned i = 0; i < 16; i++)
+                {
+                    m_Contents[i] *= rhs[i];
+                }
+
+                return (*this);
+            }
+
+            Matrix4x4<T>& operator*=(T const rhs)
+            {
+                for(unsigned i = 0; i < 16; i++)
+                {
+                    m_Contents[i] *= rhs;
+                }
+
+                return (*this);
+            }
+
+            Matrix4x4<T>& operator*=(Vector4<T> const rhs)
+            {
+                for(unsigned i = 0; i < 3; i++)
+                {
+                    m_Contents[(i * 4) + 0] *= rhs.x;
+                    m_Contents[(i * 4) + 1] *= rhs.y;
+                    m_Contents[(i * 4) + 2] *= rhs.z;
+                    m_Contents[(i * 4) + 3] *= rhs.w;
+                }
+
+                return (*this);
+            }
+
             //------------------------------------------------------------------------------
             // GETTERS
             //------------------------------------------------------------------------------
 
             /**
-            * \return The X-Axis rotation vector
-            */
+             * \return The X-Axis rotation vector
+             */
             Vector3<T> getXRotation()
             {
                 return Vector3<T>(m_Contents[0], m_Contents[1], m_Contents[2]);
             }
 
             /**
-            * \return The Y-Axis rotation vector
-            */
+             * \return The Y-Axis rotation vector
+             */
             Vector3<T> getYRotation()
             {
                 return Vector3<T>(m_Contents[4], m_Contents[5], m_Contents[6]);
             }
 
             /**
-            * \return The Z-Axis rotation vector
-            */
+             * \return The Z-Axis rotation vector
+             */
             Vector3<T> getZRotation()
             {
                 return Vector3<T>(m_Contents[8], m_Contents[9], m_Contents[10]);
             }
 
             /**
-            * \return The Position vector
-            */
+             * \return The Position vector
+             */
             Vector3<T> getPosition()
             {
                 return Vector3<T>(m_Contents[12], m_Contents[13], m_Contents[14]);
             }
 
             /**
-            * \return The element at the specified row [0-3] and colum [0-3] combination
-            */
+             * \return The element at the specified row [0-3] and colum [0-3] combination
+             */
             T getElement(unsigned const row, unsigned const column)
             {
                 if(row > 3)
@@ -210,8 +365,8 @@ namespace Ocular
             }
 
             /**
-            * \return The element at the specified index [0-15]
-            */
+             * \return The element at the specified index [0-15]
+             */
             T getElement(unsigned const index)
             {
                 if(index > 15)
@@ -271,6 +426,18 @@ namespace Ocular
             }
 
             /**
+             * Sets the X-Axis rotation vector.
+             *
+             * \param[in] vec The X-Axis rotation vector
+             */
+            void setXRotation(Vector3<T> const vec)
+            {
+                m_Contents[0] = vec.x;
+                m_Contents[1] = vec.y;
+                m_Contents[2] = vec.z;
+            }
+
+            /**
              * Sets the Y-Axis rotation vector.
              *
              * \param[in] x X component of the rotation vector
@@ -282,6 +449,18 @@ namespace Ocular
                 m_Contents[4] = x;
                 m_Contents[5] = y;
                 m_Contents[6] = z;
+            }
+
+            /**
+             * Sets the Y-Axis rotation vector.
+             *
+             * \param[in] vec The Y-Axis rotation vector
+             */
+            void setYRotation(Vector3<T> const vec)
+            {
+                m_Contents[4] = vec.x;
+                m_Contents[5] = vec.y;
+                m_Contents[6] = vec.z;
             }
 
             /**
@@ -299,6 +478,18 @@ namespace Ocular
             }
 
             /**
+             * Sets the Z-Axis rotation vector.
+             *
+             * \param[in] vec The Z-Axis rotation vector
+             */
+            void setZRotation(Vector3<T> const vec)
+            {
+                m_Contents[8]  = vec.x;
+                m_Contents[9]  = vec.y;
+                m_Contents[10] = vec.z;
+            }
+
+            /**
              * Sets the position vector.
              *
              * \param[in] x X component of the position vector
@@ -310,6 +501,18 @@ namespace Ocular
                 m_Contents[12] = x;
                 m_Contents[13] = y;
                 m_Contents[14] = z;
+            }
+
+            /**
+             * Sets the position vector.
+             *
+             * \param[in] vec The position vector
+             */
+            void setPosition(Vector3<T> const vec)
+            {
+                m_Contents[12] = vec.x;
+                m_Contents[13] = vec.y;
+                m_Contents[14] = vec.z;
             }
 
             /**
