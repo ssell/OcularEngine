9d6a61de1abda80c062d363b978debcc89e22456
ssell <ssell@knights.ucf.edu>
2015-02-01 20:52:48 -0500

    Update copyright

--- OcularCore/include/FileIO/File.hpp
+++ OcularCore/include/FileIO/File.hpp
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014 Steven T Sell (ssell@ocularinteractive.com)
+ * Copyright 2014-2015 Steven T Sell (ssell@ocularinteractive.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- OcularCore/include/Logger/ConsoleLoggerListener.hpp
+++ OcularCore/include/Logger/ConsoleLoggerListener.hpp
@@ -1,18 +1,18 @@
 /**
-* Copyright 2014 Steven T Sell (ssell@ocularinteractive.com)
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2014-2015 Steven T Sell (ssell@ocularinteractive.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #pragma once
 #ifndef __H__OCULAR_LOGGER_CONSOLE_LISTENER__H__
