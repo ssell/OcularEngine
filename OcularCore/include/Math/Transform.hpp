4c4daf66eabd662445ff79e184eee5517adcbe6a
ssell <ssell@knights.ucf.edu>
2015-05-18 01:57:08 -0400

    The actual Transform class...

---
+++ OcularCore/include/Math/Transform.hpp
@@ -0,0 +1,148 @@
+/**
+ * Copyright 2014-2015 Steven T Sell (ssell@ocularinteractive.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+#ifndef __H__OCULAR_MATH_TRANSFORM__H__
+#define __H__OCULAR_MATH_TRANSFORM__H__
+
+#include "Vector3.hpp"
+#include "Quaternion.hpp"
+
+//------------------------------------------------------------------------------------------
+
+/**
+ * \addtogroup Ocular
+ * @{
+ */
+namespace Ocular
+{
+    /**
+     * \addtogroup Math
+     * @{
+     */
+    namespace Math
+    {
+        /**
+         * \class Transform
+         */
+        class Transform
+        {
+        public:
+
+            Transform();
+            Transform(Vector3f const& position, Quaternion const& rotation, Vector3f const& scale);
+
+            ~Transform();
+
+            //------------------------------------------------------------
+            // Getters and Setters
+
+            void setPosition(Vector3f const& position);
+            Vector3f getPosition() const;
+
+            void setRotation(Quaternion const& rotation);
+            Quaternion getRotation() const;
+
+            void setScale(Vector3f const& scale);
+            Vector3f getScale() const;
+            
+            /**
+             * Retrieves the forwards (negative z-axis) vector of this transform.
+             */
+            Vector3f getForwards() const;
+            
+            /**
+             * Retrieves the upwards (positive y-axis) vector of this transform.
+             */
+            Vector3f getUp() const;
+            
+            /**
+             * Retrieves the right (positive x-axis) vector of this transform.
+             */
+            Vector3f getRight() const;
+
+            //------------------------------------------------------------
+            // Movement
+
+            /**
+             * Translates the transform in the specified direction and distance.
+             * \param[in] translation
+             */
+            void translate(Vector3f const& translation);
+            
+            /**
+             * Moves the transform along the forward vector for the given delta.
+             * Moving backwards can be achieved by specifying a negative delta.
+             *
+             * \param[in] delta
+             */
+            void moveForward(float const delta);
+            
+            /**
+             * Moves the transform along the up vector for the given delta.
+             * Moving downwards can be achieved by specifying a negative delta.
+             *
+             * \param[in] delta
+             */
+            void moveUp(float const delta);
+            
+            /**
+             * Moves the transform along the right vector for the given delta.
+             * Moving left can be achieved by specifying a negative delta.
+             *
+             * \param[in] delta
+             */
+            void moveRight(float const delta);
+            
+            //------------------------------------------------------------
+            // Rotation
+
+            /**
+             * Rotates the transform on the specified axis by the given angle.
+             *
+             * \param[in] axis  Normalized vector axis to rotate on.
+             * \param[in] angle Angles to rotate in degrees.
+             */
+            void rotate(Vector3f const& axis, float const angle);
+
+            /**
+             * Rotates the transform so that the forward vector points at the specified point.
+             *
+             * \param[in] point    Point position in space to look at.
+             * \param[in] upVector Vector that specifies the new up vector.
+             */
+            void lookAt(Vector3f const& point, Vector3f const& upVector = Vector3f::up());
+
+        protected:
+
+        private:
+
+            Vector3f   m_Position;
+            Quaternion m_Rotation;
+            Vector3f   m_Scale;
+        };
+    }
+    /**
+     * @} End of Doxygen Groups
+     */
+}
+/**
+ * @} End of Doxygen Groups
+ */
+
+//------------------------------------------------------------------------------------------
+
+#endif
---
+++ OcularCore/src/Math/Transform.cpp
@@ -0,0 +1,133 @@
+/**
+ * Copyright 2014-2015 Steven T Sell (ssell@ocularinteractive.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "Math/Transform.hpp"
+#include "Math/Euler.hpp"
+
+//------------------------------------------------------------------------------------------
+
+namespace Ocular
+{
+    namespace Math
+    {
+        //----------------------------------------------------------------------------------
+        // CONSTRUCTORS
+        //----------------------------------------------------------------------------------
+
+        Transform::Transform(Vector3f const& position, Quaternion const& rotation, Vector3f const& scale)
+            : m_Position(position), m_Rotation(rotation), m_Scale(scale)
+        {
+        
+        }
+
+        Transform::Transform()
+        {
+
+        }
+
+        Transform::~Transform()
+        {
+        
+        }
+
+        //----------------------------------------------------------------------------------
+        // PUBLIC METHODS
+        //----------------------------------------------------------------------------------
+        
+        void Transform::setPosition(Vector3f const& position)
+        {
+            m_Position = position;
+        }
+
+        Vector3f Transform::getPosition() const
+        {
+            return m_Position;
+        }
+
+        void Transform::setRotation(Quaternion const& rotation)
+        {
+            m_Rotation = rotation;
+        }
+
+        Quaternion Transform::getRotation() const
+        {
+            return m_Rotation;
+        }
+
+        void Transform::setScale(Vector3f const& scale)
+        {
+            m_Scale = scale;
+        }
+
+        Vector3f Transform::getScale() const
+        {
+            return m_Scale;
+        }
+
+        Vector3f Transform::getForwards() const
+        {
+            return m_Rotation.getZRotationAxis();
+        }
+
+        Vector3f Transform::getUp() const
+        {
+            return m_Rotation.getYRotationAxis();
+        }
+
+        Vector3f Transform::getRight() const
+        {
+            return m_Rotation.getXRotationAxis();
+        }
+
+        void Transform::translate(Vector3f const& translation)
+        {
+            m_Position += translation;
+        }
+
+        void Transform::moveForward(float const delta)
+        {
+            translate(getForwards() * delta);
+        }
+
+        void Transform::moveUp(float const delta)
+        {
+            translate(getUp() * delta);
+        }
+
+        void Transform::moveRight(float const delta)
+        {
+            translate(getRight() * delta);
+        }
+
+        void Transform::rotate(Vector3f const& axis, float const angle)
+        {
+            m_Rotation = Quaternion(axis, angle);
+        }
+
+        void Transform::lookAt(Vector3f const& point, Vector3f const& upVector)
+        {
+            m_Rotation = Quaternion::createLookAtRotation(m_Position, point, upVector);
+        }
+
+        //----------------------------------------------------------------------------------
+        // PROTECTED METHODS
+        //----------------------------------------------------------------------------------
+
+        //----------------------------------------------------------------------------------
+        // PRIVATE METHODS
+        //----------------------------------------------------------------------------------
+    }
+}
