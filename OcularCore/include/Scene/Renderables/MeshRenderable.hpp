fc25f2480e6d56ffed6b7465c9b305f37cc93c24
Steven Sell <ssell@ocularinteractive.com>
2017-03-19 04:26:35 -0400

    Updated Copyright

--- OcularCore/include/Scene/Renderables/MeshRenderable.hpp
+++ OcularCore/include/Scene/Renderables/MeshRenderable.hpp
@@ -1,5 +1,5 @@
 /**
- * Copyright 2014-2016 Steven T Sell (ssell@vertexfragment.com)
+ * Copyright 2014-2017 Steven T Sell (ssell@vertexfragment.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
